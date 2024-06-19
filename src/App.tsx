e07dcf77c660685c15d05610bfcb6be0e5877f8a
Elena-1405 <helenofspb@gmail.com>
2024-06-19 18:32:55 +0300

    App done

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
-import { Calendar } from '../src/components/calendar/calendar';
-
+import { Calendar, CalendarProps } from '../src/components/calendar/calendar';
+import { TodoList, ToDo} from '../src/components/list/todolist';
 
 function App() {
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [ todos, setTodos ] = useState<ToDo[]>([]);
+
+  const addTodo = (newTodo: ToDo) => {
+    setTodos([...todos, newTodo]);
+  };
+
+  const removeTodo = (index: number) => {
+    setTodos(todos.filter((_, i) => i !== index));
+  };
 
   const handleDateClick = (date: Date) => {
     setSelectedDate(date);
   }
   return (
-    <Calendar selectedDate={selectedDate} onDateClick={handleDateClick} />
+    <>
+      <Calendar selectedDate={selectedDate} onDateClick={handleDateClick} />
+      <TodoList date={selectedDate} todos={todos} addTodo={addTodo} removeTodo={removeTodo}/>
+    </>
   );
 }
 
