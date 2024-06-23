69b6101734249fca99b84d911cb5e88c43b377d5
Elena-1405 <helenofspb@gmail.com>
2024-06-23 13:34:28 +0300

    react-hook-form for dotolist added

--- src/components/list/todolist.tsx
+++ src/components/list/todolist.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { TodoItem } from '../item/todoitem';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 interface TodoListProps {
     date: Date;
@@ -12,20 +13,27 @@ export interface ToDo {
     title: string
 }
 
+interface FormData {
+    title: string
+}
+
 export const TodoList: React.FC<TodoListProps> = ({ date, todos, addTodo, removeTodo }) => {
-    const [newTodo, setNewTodo] = useState<ToDo>({ title:'' });
+    //const [newTodo, setNewTodo] = useState<ToDo>({ title:'' });
+    const { register, handleSubmit, formState: { errors }, reset } = useForm<FormData>();
 
-    const handleAddTodo = () => {
-        addTodo(newTodo);
-        setNewTodo({ title:'' });
+    const onSubmit: SubmitHandler<FormData> = data => {
+        addTodo({
+            title: data.title,
+        });
+        reset();
     };
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, value } = e.target;
-        setNewTodo(prevState => ({
-            ...prevState, [name]: value
-        }));
-    };
+    // const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //     const { name, value } = e.target;
+    //     setNewTodo(prevState => ({
+    //         ...prevState, [name]: value
+    //     }));
+    // };
 
     const handleRemoveTodo = (index: number) => {
         removeTodo(index);
@@ -34,18 +42,21 @@ export const TodoList: React.FC<TodoListProps> = ({ date, todos, addTodo, remove
     return (
         <div>
             <h3>To do for {date.toDateString()}</h3>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <div>
             <input 
                 type='text'
-                name='title'
-                value={newTodo.title}
-                onChange={handleChange}
+                {...register('title', { required: 'Заполните поле'})}
                 placeholder='title'
                 />
+                {errors.title && <div style={{color: 'red'}}>{errors.title.message}</div>}
+                </div>
             <button
-                onClick={handleAddTodo}
+                type='submit'
                 >
                     Add
             </button>
+            </form>
              <ul>
                 {todos.map((todo, index) => (
                     <li key={index}>
